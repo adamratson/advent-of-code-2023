e86f8dd93f1c526fdde8ab1cdb87db54a3074686
Adam Watson <awatson@scottlogic.com>
2023-12-05 13:56:54 +0000

    day one part one

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ratson/aoc-23
+
+go 1.21.4
---
+++ one.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	file, err := os.Open("one.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %s", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var result = 0
+	for scanner.Scan() {
+		var num string
+		var lastNum rune
+		var foundFirstNum = false
+		for _, char := range scanner.Text() {
+			if unicode.IsDigit(char) {
+				lastNum = char
+				if !foundFirstNum {
+					num = string(char)
+					foundFirstNum = true
+				}
+			}
+		}
+
+		num += string(lastNum)
+
+		i, err := strconv.Atoi(num)
+
+		if err != nil {
+			panic(err)
+		}
+
+		result += i
+	}
+
+	fmt.Println(result)
+}
